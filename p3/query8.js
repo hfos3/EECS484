1e750c5b4c8ed57c8bab2f2097a26af75a310b40
hfos3 <156258360+hfos3@users.noreply.github.com>
2024-01-19 16:39:27 -0500

    Update query8.js

--- p3/query8.js
+++ p3/query8.js
@@ -1,17 +1,32 @@
 // Query 8
 // Find the city average friend count per user using MapReduce.
 
+
 let city_average_friendcount_mapper = function () {
-    // TODO: Implement the map function
-};
-
-let city_average_friendcount_reducer = function (key, values) {
-    // TODO: Implement the reduce function
-};
-
-let city_average_friendcount_finalizer = function (key, reduceVal) {
+    if(this.hometown.city != null){
+        emit(this.hometown.city, {sum: this.friends.length, count: 1});
+    }
+ };
+ 
+ 
+ let city_average_friendcount_reducer = function (key, values) {
+    var fCount = 0
+    var n = 0
+    values.forEach(function(v) {
+        n += v.count;
+        fCount += v.sum;
+    });
+    return {sum: fCount,count: n};
+ };
+ 
+ 
+ let city_average_friendcount_finalizer = function (key, reduceVal) {
     // We've implemented a simple forwarding finalize function. This implementation
     // is naive: it just forwards the reduceVal to the output collection.
     // TODO: Feel free to change it if needed.
-    return reduceVal;
-};
+    if(reduceVal.count == 0){
+        return 0;
+    }
+    return reduceVal.sum / reduceVal.count
+ };
+ 
