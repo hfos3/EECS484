fe05435945e2f7c6ca4a8092670341f5cf58f168
hfos3 <156258360+hfos3@users.noreply.github.com>
2024-01-19 16:39:04 -0500

    Update query7.js

--- p3/query7.js
+++ p3/query7.js
@@ -2,16 +2,19 @@
 // Find the number of users born in each month using MapReduce
 
 let num_month_mapper = function () {
-    // TODO: Implement the map function
-};
-
-let num_month_reducer = function (key, values) {
-    // TODO: Implement the reduce function
-};
-
-let num_month_finalizer = function (key, reduceVal) {
+    emit(this.MOB,1)
+ };
+ 
+ 
+ let num_month_reducer = function (key, values) {
+    return Array.sum(values);
+ };
+ 
+ 
+ let num_month_finalizer = function (key, reduceVal) {
     // We've implemented a simple forwarding finalize function. This implementation
     // is naive: it just forwards the reduceVal to the output collection.
     // TODO: Feel free to change it if needed.
     return reduceVal;
-};
+ };
+ 
